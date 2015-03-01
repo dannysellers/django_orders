23531ae7f950980d9599ca1e099bbcd8e3ab9407
danny <dsellers90@gmail.com>
2015-03-01 13:26:10 -0800

    added all Foundation resources, django-debug-toolbar

--- order_tracker/settings.py
+++ order_tracker/settings.py
@@ -156,6 +156,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+	'debug_toolbar',
 	'tracker',
     'south',
 )
