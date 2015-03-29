1bbddbf78b9ad9becdecc4be70ca2ef0c593fbc5
danny <dsellers90@gmail.com>
2015-03-28 19:05:46 -0700

    trimmed custom login view

--- tracker/api/views/auth_views.py
+++ tracker/api/views/auth_views.py
@@ -1,65 +1,27 @@
-from rest_framework import status
-from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.serializers import AuthTokenSerializer
-from rest_framework import exceptions
+# from rest_framework import status
+# from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.serializers import AuthTokenSerializer
+# from rest_framework import exceptions
 
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-from rest_framework.generics import GenericAPIView
-from rest_auth.app_settings import LoginSerializer
+from rest_auth.views import Login
+
 from ..serializers import CustomTokenSerializer
+# from ..authentication import ExpiringTokenAuthentication
 
-from ..authentication import ExpiringTokenAuthentication
+# from datetime import datetime, timedelta
 
-from datetime import datetime, timedelta
+# from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
 
-from django.conf import settings
-from django.contrib.auth import login
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-
-import json
-from datetime import datetime, timedelta
+# import json
+# from datetime import datetime, timedelta
 
 
-class Login(GenericAPIView):
+class CustomTokenLogin(Login):
     """
-    Check the credentials and return the REST Token
-    if the credentials are valid and authenticated.
-    Calls Django Auth login method to register User ID
-    in Django session framework
-
-    Accept the following POST parameters: username, password
-    Return the REST Framework Token Object's key.
+    Overridden rest_auth.view.login to use CustomTokenSerializer
     """
-    permission_classes = (AllowAny,)
-    serializer_class = LoginSerializer
-    token_model = Token
     response_serializer = CustomTokenSerializer
-
-    def login (self):
-        self.user = self.serializer.validated_data['user']
-        self.token, created = self.token_model.objects.get_or_create(
-            user = self.user
-        )
-        if getattr(settings, 'REST_SESSION_LOGIN', True):
-            login(self.request, self.user)
-
-    def get_response (self):
-        return Response(self.response_serializer(self.token).data,
-                        status = status.HTTP_200_OK)
-
-    def get_error_response (self):
-        return Response(self.serializer.errors,
-                        status = status.HTTP_400_BAD_REQUEST)
-
-    def post (self, request, *args, **kwargs):
-        self.serializer = self.get_serializer(data = self.request.DATA)
-        if not self.serializer.is_valid():
-            return self.get_error_response()
-        self.login()
-        return self.get_response()
-
 
 
 # class ObtainExpiringAuthToken(ObtainAuthToken):
